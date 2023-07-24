bc8b2c0823b4a13e524bb2a2b02cc1f43f1a6319
imjsxoo <irmapujiandrianis@gmail.com>
2023-07-24 17:48:52 +0700

    add axios and api

--- src/component/Productcard.js
+++ src/component/Productcard.js
@@ -1,57 +1,69 @@
 import React from 'react';
 
-const Productcard = (props) => {
-    const ListProduct = [{
-        image: '/images/agoura_dt.jpg',
-        name: 'Agoura Extendable Dining Table',
-        desc: 'Acacia with Sand-Blasted Finish',
-        price: 'Rp 10.000.000'
-    },
-    {
-        image: '/images/azurro_corner_terminal_electric_notalent.jpg',
-        name: 'Azurro Corner',
-        desc: '100% Leather',
-        price: 'Rp 15.500.000'
-    },
-    {
-        image: '/images/tanami-timber-tv-entertainment-unit.jpg',
-        name: 'Tanami TV Entertainment Unit',
-        desc: 'Australian Oak with Geometric Legs',
-        price: 'Rp 8.000.000'
-    },
-    {
-        image: '/images/noida-table-lamp.jpg',
-        name: 'Noida Table Lamp',
-        desc: 'Good table Lamp',
-        price: 'Rp 2.000.000'
-    }, {
-        image: '/images/agoura_dt.jpg',
-        name: 'Agoura Extendable Dining Table',
-        desc: 'Acacia with Sand-Blasted Finish',
-        price: 'Rp 10.000.000'
-    },
-    {
-        image: '/images/azurro_corner_terminal_electric_notalent.jpg',
-        name: 'Azurro Corner',
-        desc: '100% Leather',
-        price: 'Rp 15.500.000'
-    },
-    {
-        image: '/images/tanami-timber-tv-entertainment-unit.jpg',
-        name: 'Tanami TV Entertainment Unit',
-        desc: 'Australian Oak with Geometric Legs',
-        price: 'Rp 8.000.000'
-    },
-    {
-        image: '/images/noida-table-lamp.jpg',
-        name: 'Noida Table Lamp',
-        desc: 'Good table Lamp',
-        price: 'Rp 2.000.000'
-    }]
+const Productcard = (productData) => {
+    // const ListProduct = [{
+    //     image: '/images/agoura_dt.jpg',
+    //     name: 'Agoura Extendable Dining Table',
+    //     desc: 'Acacia with Sand-Blasted Finish',
+    //     price: 'Rp 10.000.000'
+    // },
+    // {
+    //     image: '/images/azurro_corner_terminal_electric_notalent.jpg',
+    //     name: 'Azurro Corner',
+    //     desc: '100% Leather',
+    //     price: 'Rp 15.500.000'
+    // },
+    // {
+    //     image: '/images/tanami-timber-tv-entertainment-unit.jpg',
+    //     name: 'Tanami TV Entertainment Unit',
+    //     desc: 'Australian Oak with Geometric Legs',
+    //     price: 'Rp 8.000.000'
+    // },
+    // {
+    //     image: '/images/noida-table-lamp.jpg',
+    //     name: 'Noida Table Lamp',
+    //     desc: 'Good table Lamp',
+    //     price: 'Rp 2.000.000'
+    // }, {
+    //     image: '/images/agoura_dt.jpg',
+    //     name: 'Agoura Extendable Dining Table',
+    //     desc: 'Acacia with Sand-Blasted Finish',
+    //     price: 'Rp 10.000.000'
+    // },
+    // {
+    //     image: '/images/azurro_corner_terminal_electric_notalent.jpg',
+    //     name: 'Azurro Corner',
+    //     desc: '100% Leather',
+    //     price: 'Rp 15.500.000'
+    // },
+    // {
+    //     image: '/images/tanami-timber-tv-entertainment-unit.jpg',
+    //     name: 'Tanami TV Entertainment Unit',
+    //     desc: 'Australian Oak with Geometric Legs',
+    //     price: 'Rp 8.000.000'
+    // },
+    // {
+    //     image: '/images/noida-table-lamp.jpg',
+    //     name: 'Noida Table Lamp',
+    //     desc: 'Good table Lamp',
+    //     price: 'Rp 2.000.000'
+    // }]
+
+    const ListProduct = productData.productData
     let filteredProduct = ListProduct
-    if(props.count != null) {
-        filteredProduct = ListProduct.slice(0,props.count)
+    if (productData.count != null) {
+        filteredProduct = ListProduct.slice(0, props.count)
     }
+
+    const formatter = new Intl.NumberFormat('en-ID', {
+        style: 'currency',
+        currency: 'IDR',
+
+        // These options are needed to round to whole numbers if that's what you want.
+        //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+        //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+    });
+    console.log(formatter.format(2500)); /* $2,500.00 */
     return (
         <>
             {
@@ -60,8 +72,8 @@ const Productcard = (props) => {
                         <div className='flex flex-col w-[285px] max-h-[500px] bg-[#F4F5F7]'>
                             <img src={product.image} alt="image" className='h-[301px] w-[285px] object-cover' />
                             <div className='px-2 py-4'>
-                                <p className='text-2xl font-semibold '>{product.name}</p>
-                                <p className='text-base font-medium text-gray-300 py-2'>{product.desc}</p>
+                                <p className='text-2xl font-semibold '>{product.product_name}</p>
+                                <p className='text-base font-medium text-gray-300 py-2'>{product.product_subname}</p>
                                 <p className='text-xl font-semibold'>{product.price}</p>
                             </div>
                         </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,50 +1,66 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import Productcard from '@/component/Productcard'
+import Productcard from '@/component/Productcard';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
-  return (
-    <>
-      <section className='bg-hero-banner h-banner-height mx-auto flex justify-end items-center'>
-        <div className='bg-[#FFF3E3] w-1/2 px-9 py-14 float-right mr-16'>
-          <p className='text-base font-semibold'>New Arrival</p>
-          <h1 className='text-[52px] text-[#B88E2F] font-bold leading-[56px] mb-12'>Discover Our <br />New Collection</h1>
-          <button className='px-16 py-6 bg-[#B88E2F] text-white text-base font-bold'>Buy Now</button>
-        </div>
-      </section>
-      <section className='my-14'>
-        <h1 className='text-center text-[32px] font-bold'>Browse The Range</h1>
-        <div className='flex justify-center gap-5 mt-16'>
-          <div className='flex justify-center flex-col'>
-            <img src="/images/dining.png" alt="dining" className='mb-8' />
-            <span className='text-xl font-semibold text-center'>Dining</span>
+class index extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      productData: null
+    };
+  }
+  componentDidMount() {
+    axios.get('http://localhost:3001/product')
+      .then(data => {
+        this.setState({ productData: data.data })
+      })
+  }
+  render() {
+    return (
+      <>
+        <section className='bg-hero-banner h-banner-height mx-auto flex justify-end items-center'>
+          <div className='bg-[#FFF3E3] w-1/2 px-9 py-14 float-right mr-16'>
+            <p className='text-base font-semibold'>New Arrival</p>
+            <h1 className='text-[52px] text-[#B88E2F] font-bold leading-[56px] mb-12'>Discover Our <br />New Collection</h1>
+            <button className='px-16 py-6 bg-[#B88E2F] text-white text-base font-bold'>Buy Now</button>
           </div>
-          <div className='flex justify-center flex-col'>
-            <img src="/images/living.png" alt="" className='mb-8' />
-            <span className='text-xl font-semibold text-center'>Living</span>
+        </section>
+        <section className='my-14'>
+          <h1 className='text-center text-[32px] font-bold'>Browse The Range</h1>
+          <div className='flex justify-center gap-5 mt-16'>
+            <div className='flex justify-center flex-col'>
+              <img src="/images/dining.png" alt="dining" className='mb-8' />
+              <span className='text-xl font-semibold text-center'>Dining</span>
+            </div>
+            <div className='flex justify-center flex-col'>
+              <img src="/images/living.png" alt="" className='mb-8' />
+              <span className='text-xl font-semibold text-center'>Living</span>
+            </div>
+            <div className='flex justify-center flex-col'>
+              <img src="/images/bedroom.png" alt="" className='mb-8' />
+              <span className='text-xl font-semibold text-center'>Bedroom</span>
+            </div>
           </div>
-          <div className='flex justify-center flex-col'>
-            <img src="/images/bedroom.png" alt="" className='mb-8' />
-            <span className='text-xl font-semibold text-center'>Bedroom</span>
+        </section>
+
+        <section className='flex justify-center flex-col items-center'>
+          <h1 className='text-center text-[32px] font-bold mb-8'>Our Products</h1>
+          <div className='max-w-[1286px] mx-auto flex flex-wrap gap-8 justify-center'>
+            {
+              this.state.productData && <Productcard productData={this.state.productData} />
+            }
           </div>
-        </div>
-      </section>
+          <button className='mt-8 mb-16 px-20 py-3 text-base font-semibold border border-[#B88E2F] text-[#B88E2F] w-fit'>Show More</button>
+        </section>
 
-      <section className='flex justify-center flex-col items-center'>
-        <h1 className='text-center text-[32px] font-bold mb-8'>Our Products</h1>
-        <div className='max-w-[1286px] mx-auto flex flex-wrap gap-8 justify-center'>
-          <Productcard />
-        </div>
-        <button className='mt-8 mb-16 px-20 py-3 text-base font-semibold border border-[#B88E2F] text-[#B88E2F] w-fit'>Show More</button>
-      </section>
-
-      <section className='mb-12'>
-        <p className='text-xl font-semibold text-gray-400 text-center'>Share your setup with</p>
-        <h1 className='text-[40px] font-bold text-center'>#FuniroFurniture</h1>
-        <img src="/images/banner2.png" alt="" />
-      </section>
-    </>
-  )
+        <section className='mb-12'>
+          <p className='text-xl font-semibold text-gray-400 text-center'>Share your setup with</p>
+          <h1 className='text-[40px] font-bold text-center'>#FuniroFurniture</h1>
+          <img src="/images/banner2.png" alt="" />
+        </section>
+      </>
+    );
+  }
 }
+
+export default index;
